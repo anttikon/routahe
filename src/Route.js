b722cb26d17df695562c68d82e27994f80d96569
vkomulai <ville.komulainen@iki.fi>
2016-11-16 20:51:48 +0200

    Format shortname and mode of route evenly

--- src/Route.js
+++ src/Route.js
@@ -1,6 +1,6 @@
 import {bold} from 'chalk'
 import {getRoutes} from './hsl-api/hsl-api'
-import {head, last} from 'lodash'
+import {head, last, get, padEnd} from 'lodash'
 
 import {getColorByMode, getEmojiByMode, formatTime, formatDuration} from './view-utils'
 
@@ -30,13 +30,23 @@ class Route {
   }
 }
 
+function maxFieldLength(route, fieldName) {
+  return route.legs.reduce((prevMax, route) => {
+    const currLength = get(route, fieldName, "").length
+    return currLength > prevMax ? currLength : prevMax
+  }, 0)
+}
 function printRoute(route) {
   console.log('')
   const start = head(route.legs).startTime
   const end = last(route.legs).endTime
 
   printRouteInformation(start, end, route.duration)
-  route.legs.forEach(printLeg)
+
+  const maxRouteNameLength = maxFieldLength(route, 'route.shortName');
+  const maxModeLength = maxFieldLength(route, 'mode');
+
+  route.legs.forEach(leg => printLeg(leg, maxRouteNameLength, maxModeLength))
   console.log('')
 }
 
@@ -44,15 +54,13 @@ function printRouteInformation(start, end, duration) {
   console.log(`${formatTime(start)} - ${formatTime(end)} (${formatDuration(duration)})`)
 }
 
-function printLeg(leg) {
+function printLeg(leg, maxRouteNameLength, maxModeLength) {
   const color = getColorByMode(leg.mode)
   const emoji = getEmojiByMode(leg.mode)
 
-  if (leg.route && leg.route.shortName) {
-    console.log(color('  | '), `${formatTime(leg.startTime)} - ${formatTime(leg.endTime)}`, emoji, color(leg.mode), bold(leg.route.shortName), `-> ${leg.to.name}`)
-  } else {
-    console.log(color('  | '), `${formatTime(leg.startTime)} - ${formatTime(leg.endTime)}`, emoji, color(leg.mode), `-> ${leg.to.name}`)
-  }
+  const sortName = get(leg, 'route.shortName', '')
+  const shortNamePadChar = sortName.length === 0 ? '.' : ' '
+  console.log(color('  | '), `${formatTime(leg.startTime)} - ${formatTime(leg.endTime)}`, emoji, color(padEnd(leg.mode, maxModeLength)), bold(padEnd(sortName, maxRouteNameLength, shortNamePadChar)), `-> ${leg.to.name}`)
 }
 
 export default Route
