fdfdb24fcd912b9f4350b2a1ecb3f874212c12fa
Jaakko Juvonen <jaakko.juvonen@wunderdog.fi>
2016-11-17 15:30:59 +0200

    Save routes instead of addresses. Implement queries for latest and top routes.

--- src/history.js
+++ src/history.js
@@ -1,50 +1,65 @@
 import fs from 'fs'
-import _ from 'lodash'
 import {homedir} from 'os'
 
-const HISTORY_SIZE = 30
 const HISTORY_FILE = `${homedir()}/.routahe_history`
 
-async function addToHistory(...addresses) {
-	let history = await readHistory()
-	history = [...addresses, ...history]
-	history = _.uniq(history)
-	history = history.slice(0, HISTORY_SIZE)
-	return writeFileAsync(HISTORY_FILE, JSON.stringify(history))	
+async function addToHistory(from, to) {
+  const history = await readHistory()
+  const route = history.find((route) => route.from.toLowerCase() === from.toLowerCase()
+                                     && route.to.toLowerCase() === to.toLowerCase() )
+  if (route) {
+    route.count += 1
+    route.timestamp = Date.now()
+  } else {
+    const newRoute = {
+      from,
+      to,
+      timestamp: Date.now(),
+      count: 1
+    }
+    history.push(newRoute)
+  }
+  return writeFileAsync(HISTORY_FILE, JSON.stringify(history))  
 }
 
 async function readHistory() {
-	try {
-		const text = await readFileAsync(HISTORY_FILE)
-		return JSON.parse(text)
-	} catch (err) {
-		if (err.code === 'ENOENT') {  // history does't exist yet
-			return []
-		} else {
-			return err
-		}
-	}
+  try {
+    const text = await readFileAsync(HISTORY_FILE, 'utf8')
+    return JSON.parse(text)
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      return []
+    } else {
+      throw err
+    }
+  }
 }
 
-function readFileAsync(file) {
-	return new Promise((resolve, reject) => {
-		fs.readFile(file, 'utf8', (err, text) => {
-			if (err) return reject(err)
-			resolve(text)
-		})
-	})
+async function latestRoute() {
+  return getRouteBy((r1, r2) => r2.timestamp - r1.timestamp)
 }
 
-function writeFileAsync(file, data) {
-	return new Promise((resolve, reject) => {
-		fs.writeFile(file, data, (err) => {
-		  if (err) return reject(err)
-			resolve()
-		})
-	})
+async function topRoute() {
+  return getRouteBy((r1, r2) => r2.count - r1.count)  
+}
+
+async function getRouteBy(sortFunction) {
+  const history = await readHistory()
+  history.sort(sortFunction)
+  return history[0] || null
+}
+
+async function readFileAsync(file, encoding) {
+  return new Promise((resolve, reject) => fs.readFile(file, encoding, (err, text) => err ? reject(err) : resolve(text)))
+}
+
+async function writeFileAsync(file, data) {
+  return new Promise((resolve, reject) => fs.writeFile(file, data, (err) => err ? reject(err) : resolve()))
 }
 
 export default {
-	add: addToHistory,
-	get: readHistory
+  add: addToHistory,
+  get: readHistory,
+  latestRoute,
+  topRoute
 }
