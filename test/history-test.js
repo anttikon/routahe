47851a58ac343f4700a4820f808c6a40b46be491
Jaakko Juvonen <jaakko.juvonen@wunderdog.fi>
2016-11-23 07:35:00 +0200

    Add test for case insensitive matching. Test cleanup.

--- test/history-test.js
+++ test/history-test.js
@@ -4,10 +4,10 @@ import history from '../src/history'
 import fs from 'fs'
 
 describe('history', function() {
-  let fsStub, resetFs, now, clock, currentConf
+  let fsStub, resetFs, now, clock, conf
 
   beforeEach(() => {
-    currentConf = {
+    conf = {
       history: [{
         from: 'Address 1',
         to: 'Address 2',
@@ -28,7 +28,7 @@ describe('history', function() {
       }]
     }
     fsStub = {
-      readFile: sinon.spy((file, encoding, cb) => cb(null, JSON.stringify(currentConf))),
+      readFile: sinon.spy((file, encoding, cb) => cb(null, JSON.stringify(conf))),
       writeFile: sinon.spy((file, data, cb) => cb(null))
     }
     resetFs = history.__Rewire__('fs', fsStub)
@@ -50,7 +50,7 @@ describe('history', function() {
 
     it('should add new route to history', async () => {
       const expectedConf = {
-        history: [...currentConf.history, {
+        history: [...conf.history, {
           from: 'New address 1',
           to: 'New address 2',
           count: 1,
@@ -70,7 +70,7 @@ describe('history', function() {
             count: 2,
             timestamp: now
           },
-          ...currentConf.history.slice(1)
+          ...conf.history.slice(1)
         ]
       }
       await history.add('Address 1', 'Address 2')
@@ -78,6 +78,22 @@ describe('history', function() {
       assert.deepEqual(JSON.parse(newConf), expectedConf)
     })
   })
+
+  it('should use case insensitive address matching', async () => {
+      const expectedConf = {
+        history: [{
+            from: 'Address 1',
+            to: 'Address 2',
+            count: 2,
+            timestamp: now
+          },
+          ...conf.history.slice(1)
+        ]
+      }
+      await history.add('ADDRESS 1', 'aDdResS 2')
+      const newConf = fsStub.writeFile.getCall(0).args[1]
+      assert.deepEqual(JSON.parse(newConf), expectedConf)
+    })
 
   describe('readHistory()', () => {
     it('should read history from configuration file', async () => {
@@ -88,7 +104,7 @@ describe('history', function() {
 
     it('should return route history', async () => {
       const historyData = await history.get()
-      assert.deepEqual(historyData, currentConf.history)
+      assert.deepEqual(historyData, conf.history)
     })
 
     it('should return empty history if configuration file does not exist', async () => {
@@ -100,17 +116,17 @@ describe('history', function() {
 
   describe('topRoute()', () => {
     it('should return route with highest usage count', async () => {
-      const top = currentConf.history[2]
+      const top = conf.history[2]
       let route = await history.topRoute()
       assert.deepEqual(route, top)
 
-      currentConf.history = [top, currentConf.history[1], currentConf.history[0]]
+      conf.history = [top, conf.history[1], conf.history[0]]
       route = await history.topRoute()
       assert.deepEqual(route, top)
     })
 
     it('should return null if history is empty', async () => {
-      currentConf.history = []
+      conf.history = []
       const route = await history.topRoute()
       assert.isNull(route)
     })
@@ -118,17 +134,17 @@ describe('history', function() {
 
   describe('latestRoute()', () => {
     it('should return route with latest timestamp', async () => {
-      const latest = currentConf.history[2]
+      const latest = conf.history[2]
       let route = await history.latestRoute()
       assert.deepEqual(route, latest)
 
-      currentConf.history = [latest, currentConf.history[1], currentConf.history[0]]
+      conf.history = [latest, conf.history[1], conf.history[0]]
       route = await history.latestRoute()
       assert.deepEqual(route, latest)
     })
 
     it('should return null if history is empty', async () => {
-      currentConf.history = []
+      conf.history = []
       const route = await history.latestRoute()
       assert.isNull(route)
     })
