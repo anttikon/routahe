33a3edd5f6b2347d8435b7b86b5f2dc0d8f10495
Antti Kontoniemi <antti.kontoniemi@gmail.com>
2016-11-24 17:42:11 +0200

    Add custom --help

--- src/routahe.js
+++ src/routahe.js
@@ -2,11 +2,32 @@ import program from 'commander'
 import Route from './Route'
 import {getLocation} from './Location'
 import {parseArguments} from './argument-parser'
+import {gray} from 'chalk'
 
 const action = async function(opts) {
   const [fromLocation, toLocation] = await Promise.all([getLocation(opts.addressFrom), getLocation(opts.addressTo)])
   await new Route(fromLocation, toLocation, opts.dateTime, opts.arriveBy).printRoutes()
 }
+
+program.on('--help', function() {
+  console.log('  Examples:');
+  console.log('');
+  console.log(gray('    Default from - to usage'))
+  console.log('    $ routahe kamppi pasila')
+  console.log('');
+  console.log(gray('    Get current location by close wifi access points'))
+  console.log('    $ routahe pasila')
+  console.log('');
+  console.log(gray('    Specify departure time'))
+  console.log('    $ routahe kamppi pasila 5:30')
+  console.log('');
+  console.log(gray('    Specify arrival time with @'))
+  console.log('    $ routahe kamppi pasila @12:30')
+  console.log('');
+  console.log(gray('    Specify date with time'))
+  console.log('    $ routahe kamppi pasila 12:30 24.12.2016')
+  console.log('')
+})
 
 program
   .arguments('<arg1> [arg2] [arg3] [arg4]')
