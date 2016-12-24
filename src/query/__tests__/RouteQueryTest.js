dab59c854fd0e2bfc913384dcb6558ff65ff3794
Antti Kontoniemi <antti.kontoniemi@gmail.com>
2016-12-24 14:26:32 +0200

    Use next wednesday as query date in RouteQueryTest since HSL API does not return search results of date is too far in history. Also updated snapshot to equal real result.

--- src/query/__tests__/RouteQueryTest.js
+++ src/query/__tests__/RouteQueryTest.js
@@ -4,27 +4,42 @@ import RouteQuery from '../RouteQuery'
 describe('HSL-API', function() {
   jasmine.DEFAULT_TIMEOUT_INTERVAL = 50000
 
+  const nextWednesday = moment().day(10).hour(15).minute(13)
+
   it('should be able to get route', async() => {
-    const date = moment(1481728405559)
     const routes = await new RouteQuery().fetch({
       from: {label: 'Kamppi, Helsinki', lon: 24.931861, lat: 60.169067}, to: {
         label: 'Kannistontie 2, Vantaa',
         lon: 24.841085,
         lat: 60.322768
-      }, date, arriveBy: false
+      }, date: nextWednesday, arriveBy: false
     })
-    expect(routes).toMatchSnapshot()
+
+    const filteredRoutes = routes.map(route => removeRouteTimeAttributes(route))
+
+    expect(filteredRoutes).toMatchSnapshot()
   })
 
   it('should be able to get route with arriveBy', async() => {
-    const date = moment(1481728405559)
     const routes = await new RouteQuery().fetch({
       from: {label: 'Kamppi, Helsinki', lon: 24.931861, lat: 60.169067}, to: {
         label: 'Kannistontie 2, Vantaa',
         lon: 24.841085,
         lat: 60.322768
-      }, date, arriveBy: true
+      }, date: nextWednesday, arriveBy: true
     })
-    expect(routes).toMatchSnapshot()
+
+    const filteredRoutes = routes.map(route => removeRouteTimeAttributes(route))
+    expect(filteredRoutes).toMatchSnapshot()
   })
+
+  function removeRouteTimeAttributes(route) {
+    route.legs = route.legs.map(leg => removeLegTimeAttributes(leg))
+    return route
+  }
+
+  function removeLegTimeAttributes(leg) {
+    const {duration, distance, mode, from, to, route} = leg
+    return {duration, distance, mode, from, to, route}
+  }
 })
