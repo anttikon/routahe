cfe83fe2c795b2202330864f07d5dff53f9829d3
Antti Kontoniemi <antti.kontoniemi@wunderdog.fi>
2019-07-28 22:32:28 +0300

    Do not use encodeURIComponent since it will cause problems with API. For an example "Hylkeenpyytäjänkatu" did not return what you'd expect. Also add size and lang to params and refactor a bit

--- src/hsl/location.js
+++ src/hsl/location.js
@@ -1,25 +1,30 @@
 import fetch from 'node-fetch'
 import querystring from 'querystring'
 
-const getHslLocations = async (query) => {
+const getHslLocation = async (query) => {
   const params = {
-    text: encodeURIComponent(query),
+    text: query,
     'boundary.rect.min_lat': 59.9,
     'boundary.rect.max_lat': 60.45,
     'boundary.rect.min_lon': 24.3,
     'boundary.rect.max_lon': 25.5,
+    size: 1,
+    lang: 'fi',
   }
   const response = await fetch(`http://api.digitransit.fi/geocoding/v1/search?${querystring.stringify(params)}`)
   return response.json()
 }
 
 const mapFeature = feature => {
+  if (!feature) {
+    return feature
+  }
   const [lon, lat] = feature.geometry.coordinates
   const label = feature.properties.label
   return { label, lon, lat }
 }
 
 export const getLocationByString = async query => {
-  const hslLocations = await getHslLocations(query)
-  return hslLocations.features ? hslLocations.features.map(mapFeature)[0] : null
+  const hslLocation = await getHslLocation(query)
+  return mapFeature(hslLocation.features?.[0])
 }
